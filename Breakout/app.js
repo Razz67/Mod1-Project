84dafddf0ce5ac0d50e567207c606fe0384303ff
Razz67 <srasberry00@gmail.com>
2022-09-14 12:07:33 -0400

    Add phaser.js file

--- Breakout/app.js
+++ Breakout/app.js
@@ -1,12 +1,12 @@
 let ball;
 let paddle;
-let bricks;
-let scoreText;
-let livesText;
-let startButton;
+let block;
+let scoreTxt;
+let livesTxt;
+let startBtn;
 let rotation;
-let gameOverText;
-let wonTheGameText;
+let gameOvertxt;
+let gameWonTxt;
 
 let score = 0;
 let lives = 3;
@@ -24,7 +24,7 @@ const config = {
 	physics: {
 		default: "arcade",
 		arcade: {
-			// debug: true,
+			debug: false, // set to true to show the debug lines
 			checkCollision: {
 				up: true,
 				down: false,
@@ -43,37 +43,43 @@ const config = {
 const game = new Phaser.Game(config);
 
 function preload() {
+	this.load.image("background", "images/background2.png");
 	this.load.image("paddle", "images/paddle.png");
-	this.load.image("brick", "images/block2.png");
+	this.load.image("block", "images/block.png");
 	this.load.image("destroyed", "images/destroyed.png");
 	this.load.image("ball", "images/ball.png");
 }
 
 function create() {
-	paddle = this.physics.add.image(this.cameras.main.centerX, this.game.config.height - 50, "paddle").setImmovable();
+	this.add.image(50, 50, "background").setOrigin(0, 0);
+	paddle = this.physics.add
+		.image(this.cameras.main.centerX, this.game.config.height - 50, "paddle")
+		.setImmovable();
 
-	ball = this.physics.add.image(this.cameras.main.centerX, this.game.config.height - 100, "ball")
+	ball = this.physics.add
+		.image(this.cameras.main.centerX, this.game.config.height - 100, "ball")
 		.setCollideWorldBounds(true)
 		.setBounce(1);
 
-	bricks = this.physics.add.staticGroup({
-		key: "brick",
-		frameQuantity: 20,
+
+	// Add Emojies
+	block = this.physics.add.staticGroup({
+		key: "block",
+		frameQuantity: 30, // determines the number of emojies added to the game
 		gridAlign: {
 			width: 10,
 			cellWidth: 60,
 			cellHeight: 60,
-			x: this.cameras.main.centerX - 277.5,
-			y: 100,
+			x: this.cameras.main.centerX - 277.5, y: 100,
 		},
 	});
 
-	scoreText = this.add.text(20, 20, "Score: 0", textStyle);
-	livesText = this.add
+	scoreTxt = this.add.text(20, 20, "Score: 0", textStyle);
+	livesTxt = this.add
 		.text(this.game.config.width - 20, 20, "Lives: " + lives, textStyle)
 		.setOrigin(1, 0);
 
-	gameOverText = this.add
+	gameOvertxt = this.add
 		.text(
 			this.cameras.main.centerX,
 			this.cameras.main.centerY,
@@ -85,42 +91,43 @@ function create() {
 		.setStyle({ backgroundColor: "#111", fill: "#e74c3c" })
 		.setVisible(false);
 
-	wonTheGameText = this.add.text(this.cameras.main.centerX, this.cameras.main.centerY, "You won the game!", textStyle)
+	gameWonTxt = this.add
+		.text(
+			this.cameras.main.centerX,
+			this.cameras.main.centerY,
+			"You won the game!",
+			textStyle
+		)
 		.setOrigin(0.5)
 		.setPadding(10)
 		.setStyle({ backgroundColor: "#111", fill: "#27ae60" })
 		.setVisible(false);
 
-	startButton = this.add
-		.text(
-			this.cameras.main.centerX,
-			this.cameras.main.centerY,
-			"Start game",
-			textStyle
-		)
+	startBtn = this.add
+		.text(this.cameras.main.centerX, this.cameras.main.centerY, "Start game", textStyle)
 		.setOrigin(0.5)
 		.setPadding(10)
 		.setStyle({ backgroundColor: "#111" })
 		.setInteractive({ useHandCursor: true })
 		.on("pointerdown", () => startGame.call(this))
-		.on("pointerover", () => startButton.setStyle({ fill: "#f39c12" }))
-		.on("pointerout", () => startButton.setStyle({ fill: "#FFF" }));
+		.on("pointerover", () => startBtn.setStyle({ fill: "#f39c12" }))
+		.on("pointerout", () => startBtn.setStyle({ fill: "#FFF" }));
 
-	this.physics.add.collider(ball, bricks, brickHit, null, this);
+	this.physics.add.collider(ball, block, blockHit, null, this);
 	this.physics.add.collider(ball, paddle, paddleHit, null, this);
 }
 
 function update() {
 	if (rotation) {
 		ball.rotation =
-			rotation === "left" ? ball.rotation - 0.05 : ball.rotation + 0.02;
+			rotation === "left" ? ball.rotation - 0.05 : ball.rotation + 0.05;
 	}
 
 	if (ball.y > paddle.y) {
 		lives--;
 
 		if (lives > 0) {
-			livesText.setText(`Lives: ${lives}`);
+			livesTxt.setText(`Lives: ${lives}`);
 
 			ball
 				.setPosition(this.cameras.main.centerX, this.game.config.height - 100)
@@ -128,7 +135,7 @@ function update() {
 		} else {
 			ball.destroy();
 
-			gameOverText.setVisible(true);
+			gameOvertxt.setVisible(true);
 		}
 	}
 }
@@ -145,10 +152,44 @@ function paddleHit(ball, paddle) {
 		ball.setVelocityX(20 * diff);
 		rotation = "right";
 	} else {
-		ball.setVelocityX(2 + Math.random() * 10);
+		ball.setVelocityX(2 + Math.random() * 10); // sets a random velocity of the ball afgter it hits the paddle
 	}
 }
 
-let update = () => {
-	console.log("update");
-};
+function blockHit(ball, block) {
+	block.setTexture("destroyed");  // changes the texture of the block to destroyed.png
+	score += 5;
+	scoreTxt.setText(`Score: ${score}`);
+
+	this.tweens.add({  // handles the animation of the block when hit
+		targets: block,
+		scaleX: 0,
+		scaleY: 0,
+		ease: "Power1",
+		duration: 500,
+		delay: 250,
+		angle: 180,
+		onComplete: () => {
+			block.destroy();
+
+			if (block.countActive() == 0) {
+				ball.destroy();
+				gameWonTxt.setVisible(true);
+			}
+		},
+	});
+}
+
+function startGame() {
+	startBtn.destroy();
+	ball.setVelocity(-300, -150);
+	rotation = "right";
+
+	this.input.on("pointermove", (pointer) => {
+		paddle.x = Phaser.Math.Clamp(
+			pointer.x,
+			paddle.width / 2,
+			this.game.config.width - paddle.width / 2
+		);
+	});
+}
