011192c8ec438c9692c28f79a00dd01263a7cf16
Razz67 <srasberry00@gmail.com>
2022-09-19 08:04:36 -0400

    95 percent complete

---
+++ Breakout/js/preload.js
@@ -0,0 +1,31 @@
+
+const BG_IMG = new Image();
+BG_IMG.src = "images/background.jpg";
+
+const LEVEL_IMG = new Image();
+LEVEL_IMG.src = "images/level.png";
+
+const LIFE_IMG = new Image();
+LIFE_IMG.src = "images/life.png";
+
+const SCORE_IMG = new Image();
+SCORE_IMG.src = "images/score.png";
+
+
+
+// LOAD SOUNDS
+const WALL_HIT = new Audio();
+WALL_HIT.src = "audio/wall.mp3";
+
+const LIFE_LOST = new Audio();
+LIFE_LOST.src = "audio/life_lost.mp3";
+
+const PADDLE_HIT = new Audio();
+PADDLE_HIT.src = "audio/paddle_hit.mp3";
+
+const WIN = new Audio();
+WIN.src = "audio/win.mp3";
+
+const BRICK_HIT = new Audio();
+BRICK_HIT.src = "audio/brick_hit.mp3";
+
